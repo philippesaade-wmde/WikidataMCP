880ab568cf18b39108bb0f024afc24d4aba8c2be
Philippe Saade <philippe.saade@wikimedia.de>
2025-09-26 19:22:32 +0200

    Upgrade FastMCP

--- main.py
+++ main.py
@@ -5,25 +5,24 @@ from wikidataMCP.tools import mcp
 
 templates = Jinja2Templates(directory="templates")
 
-# # Homepage
-# @mcp.custom_route("/", methods=["GET"])
-# async def docs(request):
-#     tool_names = await mcp.get_tools()
-#     tool_names = tool_names.keys()
+# Homepage
+@mcp.custom_route("/", methods=["GET"])
+async def docs(request):
+    tool_names = await mcp.get_tools()
+    tool_names = tool_names.keys()
 
-#     prompt = await mcp.get_prompt("explore_wikidata")
-#     prompt = await prompt.render({"query": "[User Prompt]"})
-#     prompt = markdown(prompt[0].content.text)
+    prompt = await mcp.get_prompt("explore_wikidata")
+    prompt = await prompt.render({"query": "[User Prompt]"})
+    prompt = markdown(prompt[0].content.text)
 
-#     return templates.TemplateResponse(
-#         "docs.html",
-#         {
-#             "request": request,
-#             "tools": tool_names,
-#             "prompt": prompt,
-#         },
-#     )
-
+    return templates.TemplateResponse(
+        "docs.html",
+        {
+            "request": request,
+            "tools": tool_names,
+            "prompt": prompt,
+        },
+    )
 
 # Build the MCP ASGI app
 app = mcp.http_app()
