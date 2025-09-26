bd804e5c481fec2d1bcd36059f8ca6637ec4dd2e
Philippe Saade <philippe.saade@wikimedia.de>
2025-09-26 17:31:32 +0200

    Test without homepage

--- main.py
+++ main.py
@@ -5,28 +5,28 @@ from wikidataMCP.tools import mcp
 
 templates = Jinja2Templates(directory="templates")
 
-# Homepage
-@mcp.custom_route("/", methods=["GET"])
-async def docs(request):
-    tool_names = await mcp.get_tools()
-    tool_names = tool_names.keys()
+# # Homepage
+# @mcp.custom_route("/", methods=["GET"])
+# async def docs(request):
+#     tool_names = await mcp.get_tools()
+#     tool_names = tool_names.keys()
 
-    prompt = await mcp.get_prompt("explore_wikidata")
-    prompt = await prompt.render({"query": "[User Prompt]"})
-    prompt = markdown(prompt[0].content.text)
+#     prompt = await mcp.get_prompt("explore_wikidata")
+#     prompt = await prompt.render({"query": "[User Prompt]"})
+#     prompt = markdown(prompt[0].content.text)
 
-    return templates.TemplateResponse(
-        "docs.html",
-        {
-            "request": request,
-            "tools": tool_names,
-            "prompt": prompt,
-        },
-    )
+#     return templates.TemplateResponse(
+#         "docs.html",
+#         {
+#             "request": request,
+#             "tools": tool_names,
+#             "prompt": prompt,
+#         },
+#     )
 
 
 # Build the MCP ASGI app
-app = mcp.http_app("/mcp")
+app = mcp.http_app()
 
 if __name__ == "__main__":
     # Run: python server_fastapi.py
